4285d2addb0edad49193926362daa171b961b2f8
NhatTanVu <onchua2006@gmail.com>
2018-05-10 10:40:43 +0800

    Override StorefrontUrlManager class to allow parsing multiple hostnames and wildcards.

---
+++ src/Sitecore.Support.36773/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.36773")]
+[assembly: AssemblyProduct("Sitecore.Support.36773")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.36773/StorefrontUrlManager.cs
@@ -0,0 +1,63 @@
+﻿using Sitecore.Commerce.XA.Foundation.Common;
+using Sitecore.Commerce.XA.Foundation.Common.UrlManager;
+using System;
+using System.Globalization;
+
+namespace Sitecore.Support.Commerce.XA.Foundation.Common.UrlManager
+{
+  public class StorefrontUrlManager : Sitecore.Commerce.XA.Foundation.Common.UrlManager.StorefrontUrlManager
+  {
+    public StorefrontUrlManager(ISiteContext siteContext) : base(siteContext)
+    {
+    }
+
+    public override UrlBuilder GetStorefrontUrl(string url)
+    {
+      string text;
+      try
+      {
+        return new UrlBuilder(new Uri(url));
+      }
+      catch (Exception)
+      {
+        text = string.Format(CultureInfo.InvariantCulture, "/{0}", url.Trim(new char[]
+        {
+          '/'
+        }));
+      }
+      if (!string.IsNullOrEmpty(base.SiteContext.VirtualFolder.Trim(new char[]
+      {
+        '/'
+      })) && !text.Trim(new char[]
+      {
+        '/'
+      }).StartsWith(base.SiteContext.VirtualFolder.Trim(new char[]
+      {
+        '/'
+      }), StringComparison.OrdinalIgnoreCase) && !text.StartsWith(string.Format(CultureInfo.InvariantCulture, "/{0}/", base.SiteContext.VirtualFolder.Trim(new char[]
+      {
+        '/'
+      })), StringComparison.OrdinalIgnoreCase))
+      {
+        text = string.Format(CultureInfo.InvariantCulture, "/{0}/{1}", base.SiteContext.VirtualFolder.Trim(new char[]
+        {
+          '/'
+        }), text.Trim(new char[]
+        {
+          '/'
+        }));
+      }
+      string hostName = Context.Site.HostName;
+      string uriString;
+      if (!string.IsNullOrEmpty(hostName) && !hostName.Contains("*") && !hostName.Contains("|"))
+      {
+        uriString = string.Format(CultureInfo.InvariantCulture, "{0}://{1}", StorefrontUrlManager.CurrentUrl.Scheme, hostName);
+      }
+      else
+      {
+        uriString = string.Format(CultureInfo.InvariantCulture, "{0}://{1}", StorefrontUrlManager.CurrentUrl.Scheme, StorefrontUrlManager.CurrentUrl.Host);
+      }
+      return new UrlBuilder(new Uri(new Uri(uriString), text));
+    }
+  }
+}
